3f4e83ae278fb8ebe6693dcd3f0d8b0dfbaee7a1
Mary-Maina <richellwanjiru@gmail.com>
2024-07-19 12:26:42 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,8 @@
 #install a flask from pip3
-exec { 'install flask':
-  command =>  'pip3 install flask==2.1.0',
-  path    =>  '/usr/bin/',
-  unless  =>  'pip3 list | grep flask',
+package { 'python3-pip':
+  ensure => present,
+}
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
