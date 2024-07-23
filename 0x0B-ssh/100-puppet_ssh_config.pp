9a43d481eb66732c9a5cf4c7a4603d58526150d8
Mary-Maina <richellwanjiru@gmail.com>
2024-07-23 07:27:26 -0700

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,14 +5,14 @@ file {'/etc/ssh/ssh_config':
   ensure => 'present',
 }
 file_line {'remove password auth':
-  path	=> '/etc/ssh/ssh_config',
-  line	=> 'PasswordAuthentication no',
-  match	=> 'PasswordAuthentication yes',
+  path	  => '/etc/ssh/ssh_config',
+  line	  => 'PasswordAuthentication no',
+  match   => 'PasswordAuthentication yes',
   replace => 'true',
 }
 file_line {'Use a identity file':
   ensure  => 'present',
   path    => '/etc/ssh/ssh_config',
-  line    => 'IdentityFile ~/.ssh/config',
+  line    => 'IdentityFile ~/.ssh/school',
   match   => '^identityFile',
 }
